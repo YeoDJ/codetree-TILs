221372935079b6e31d0b4e94a4eca209e937a8d3
YeoDJ <98148615+YeoDJ@users.noreply.github.com>
2023-11-12 18:04:12 +0900

    Title: 점 개수 세기, Time: 104ms, Memory: 4MB, Status: Wrong Answer - CodeTree

---
+++ 231112/점 개수 세기/count-number-of-points.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <set>
+#include <unordered_map>
+using namespace std;
+
+int n, q;
+set<int> arr;
+unordered_map<int, int> MAP;
+
+void input() {
+    cin >> n >> q;
+    for (int i = 0; i < n; i++) {
+        int num;
+        cin >> num;
+        arr.insert(num);
+    }
+    int idx = 1;
+    for (auto &&i : arr)
+        MAP[i] = idx++;
+}
+
+int solution(int a, int b) {
+    auto sit = arr.lower_bound(a);
+    if (sit == arr.end())
+        return 0;
+    auto eit = arr.lower_bound(b);
+    if (eit == arr.end() || *eit > b)
+        eit--;
+    return (eit == arr.end() || *sit > *eit) ? 0 : MAP[*eit] - MAP[*sit] + 1;
+}
+
+int main() {
+    input();
+    for (int i = 0; i < q; i++) {
+        int a, b;
+        cin >> a >> b;
+        cout << solution(a, b) << endl;
+    }
+
+    return 0;
+}
