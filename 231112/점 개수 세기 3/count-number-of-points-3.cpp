2c75eb44e2fd7b70805441a4cce7582d9396828b
YeoDJ <98148615+YeoDJ@users.noreply.github.com>
2023-11-12 13:36:48 +0900

    Title: 점 개수 세기 3, Time: 414ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231112/점 개수 세기 3/count-number-of-points-3.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <queue>
+#include <set>
+#include <unordered_map>
+using namespace std;
+
+int n, q;
+set<int> arr;
+unordered_map<int, int> MAP;
+
+void input() {
+    cin >> n >> q;
+    // treeset으로 주어진 Node 값 대입
+    for (int i = 0; i < n; i++) {
+        int num;
+        cin >> num;
+        arr.insert(num);
+    }
+    // 이 순서대로 hashmap에 Node 번호 부여(val, Node_num)
+    int idx = 1;
+    for (auto &&i : arr)
+        MAP[i] = idx++;
+}
+
+int main() {
+    input();
+    for (int i = 0; i < q; i++) {
+        int a, b;
+        cin >> a >> b;
+        // a 이상으로 시작하는 iter
+        auto it_start = arr.lower_bound(a);
+        // b 이하로 끝나는 iter
+        auto it_end = arr.lower_bound(b);
+		if (it_end == arr.end())
+			it_end--;
+        // Node 번호로 개수 판별
+        cout << MAP[*it_end] - MAP[*it_start] + 1 << endl;
+    }
+
+    return 0;
+}
