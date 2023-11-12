85ce96d5fe5c7ec78a3e2f933fb44ea981304baa
YeoDJ <98148615+YeoDJ@users.noreply.github.com>
2023-11-12 18:22:07 +0900

    Title: 점 개수 세기 2, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231112/점 개수 세기 2/count-number-of-points-2.cpp
+++ 231112/점 개수 세기 2/count-number-of-points-2.cpp
@@ -1,60 +1,38 @@
 #include <iostream>
-#include <map>
 #include <set>
 #include <unordered_map>
-#define IT (*it).second
 using namespace std;
 
-typedef map<int, set<int>>::iterator iter;
 int n, q;
-// X 좌표에 따른 Y 좌표 값
-map<int, set<int>> arr;
-// X 좌표별 Y 좌표에 따른 Node 번호 부여
-map<int, unordered_map<int, int>> MAP;
+set<int> arr;
+unordered_map<int, int> MAP;
 
 void input() {
     cin >> n >> q;
     for (int i = 0; i < n; i++) {
-        int x, y;
-        cin >> x >> y;
-        arr[x].insert(y);
+        int num;
+        cin >> num;
+        arr.insert(num);
     }
-    for (auto &&i : arr) {
-        int idx = 1;
-        for (auto &&j : i.second)
-            MAP[i.first][j] = idx++;
-    }
+    int idx = 1;
+    for (auto &&i : arr)
+        MAP[i] = idx++;
 }
 
-int solution(int x1, int y1, int x2, int y2) {
-    // x1 이상 x2 이하의 iter 구하기
-    int cnt = 0;
-    iter s_iter_x = arr.lower_bound(x1);
-    if (s_iter_x == arr.end())
+int solution(int a, int b) {
+    auto sit = arr.lower_bound(a);
+    if (sit == arr.end())
         return 0;
-    iter e_iter_x = --arr.upper_bound(x2);
-    if (e_iter_x == arr.end() || (*s_iter_x).first > (*e_iter_x).first)
-        return 0;
-
-    // y1 이상 y2 이하의 iter 구하기
-    for (iter it = s_iter_x; it != arr.end(); it++) {
-        auto s_iter_y = IT.lower_bound(y1);
-        if (s_iter_y == IT.end())
-            continue;
-        auto e_iter_y = --IT.upper_bound(y2);
-        cnt += (e_iter_y == IT.end() || *s_iter_y > *e_iter_y) ? 0 : MAP[(*it).first][*e_iter_y] - MAP[(*it).first][*s_iter_y] + 1;
-        if (it == e_iter_x)
-            break;
-    }
-    return cnt;
+    auto eit = --arr.upper_bound(b);
+    return (eit == arr.begin() || *sit > *eit) ? 0 : MAP[*eit] - MAP[*sit] + 1;
 }
 
 int main() {
     input();
     for (int i = 0; i < q; i++) {
-        int x1, y1, x2, y2, cnt = 0;
-        cin >> x1 >> y1 >> x2 >> y2;
-        cout << solution(x1, y1, x2, y2) << endl;
+        int a, b;
+        cin >> a >> b;
+        cout << solution(a, b) << endl;
     }
     return 0;
 }
