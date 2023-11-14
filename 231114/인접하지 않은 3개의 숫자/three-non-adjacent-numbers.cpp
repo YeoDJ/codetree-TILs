8a44f07859e383260bf478a19ae02248ffc523cb
YeoDJ <98148615+YeoDJ@users.noreply.github.com>
2023-11-14 21:03:43 +0900

    Title: 인접하지 않은 3개의 숫자, Time: 40ms, Memory: 1MB, Status: Passed - CodeTree

--- 231114/인접하지 않은 3개의 숫자/three-non-adjacent-numbers.cpp
+++ 231114/인접하지 않은 3개의 숫자/three-non-adjacent-numbers.cpp
@@ -11,6 +11,8 @@ void input() {
     arr = L = R = vector<int>(n, 0);
     for (auto &&i : arr)
         cin >> i;
+
+    // 배열을 왼쪽(오른쪽)부터 계속 탐색하며 얻은 최대값
     L[0] = arr[0], R[n - 1] = arr[n - 1];
     for (int i = 1; i < n; i++) {
         L[i] = max(L[i - 1], arr[i]);
