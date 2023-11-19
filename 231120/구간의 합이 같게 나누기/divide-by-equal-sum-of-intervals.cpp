4dd6a50b3d301e5285c2b0c8b36d72db0184e81e
YeoDJ <98148615+YeoDJ@users.noreply.github.com>
2023-11-20 00:38:53 +0900

    Title: 구간의 합이 같게 나누기, Time: 95ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231120/구간의 합이 같게 나누기/divide-by-equal-sum-of-intervals.cpp
+++ 231120/구간의 합이 같게 나누기/divide-by-equal-sum-of-intervals.cpp
@@ -47,8 +47,11 @@ int main() {
     for (auto &&i : idx_2L) {
         iter it1 = idx_L.lower_bound(i);
         it1--;
-        iter it2 = idx_2R.upper_bound(i);
-        ans += (distance(idx_L.begin(), it1) + 1) * distance(idx_R.upper_bound(*it2), idx_R.end());
+        iter it2 = idx_2R.lower_bound(i);
+        if (*it2 == i)
+            it2++;
+        if (*it2 - i == 1)
+            ans += (distance(idx_L.begin(), it1) + 1) * distance(idx_R.upper_bound(*it2), idx_R.end());
     }
     cout << ans;
     return 0;
