39bac30642e480509549875727a3a76d6beb337a
YeoDJ <98148615+YeoDJ@users.noreply.github.com>
2023-11-19 23:58:00 +0900

    Title: 구간의 합이 같게 나누기, Time: 97ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231119/구간의 합이 같게 나누기/divide-by-equal-sum-of-intervals.cpp
+++ 231119/구간의 합이 같게 나누기/divide-by-equal-sum-of-intervals.cpp
@@ -7,12 +7,12 @@
 using namespace std;
 
 int n, sum = 0, ans = 0;
-vector<int> arr, L, R;
+vector<int> arr;
 set<int> idx_L, idx_2L, idx_R, idx_2R;
 
 void input() {
     cin >> n;
-    arr = L = R = vector<int>(n, 0);
+    arr = vector<int>(n, 0);
     for (auto &&i : arr) {
         cin >> i;
         sum += i;
@@ -32,7 +32,6 @@ void LR_init() {
             idx_2L.insert(i);
         if (n - 1 > j && j >= 2 && sumR == sum * 2)
             idx_2R.insert(j);
-        L[i] = sumL, R[j] = sumR;
     }
 }
 
@@ -43,15 +42,14 @@ int main() {
         return 0;
     }
 
+    // 탐색 순서: idx_2L, idx_L, idx_2R, idx_R
     LR_init();
-    // 탐색 순서: idx_L, idx_2L, idx_2R, idx_R
-    for (auto &&i : idx_L) {
-        iter it1 = idx_2L.upper_bound(i);
-        for (iter it = it1; it != idx_2L.end(); it++) {
-            iter it2 = idx_2R.upper_bound(*it);
-            if (*it2 - *it == 1)
-                ans += distance(idx_R.upper_bound(*it2), idx_R.end());
-        }
+    for (auto &&i : idx_2L) {
+        iter it1 = idx_L.lower_bound(i);
+        it1--;
+        iter it2 = idx_2R.upper_bound(i);
+        if (it1 != idx_L.end() && it2 != idx_2R.end() && *it2 - i == 1)
+            ans += distance(idx_R.upper_bound(*it2), idx_R.end()) * (distance(idx_L.begin(), it1) + 1);
     }
     cout << ans;
     return 0;
