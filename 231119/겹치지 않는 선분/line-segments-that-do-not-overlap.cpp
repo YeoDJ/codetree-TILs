64f74ca7d20ee760977883286d70f3651ac054fa
YeoDJ <98148615+YeoDJ@users.noreply.github.com>
2023-11-19 12:55:12 +0900

    Title: 겹치지 않는 선분, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231119/겹치지 않는 선분/line-segments-that-do-not-overlap.cpp
@@ -0,0 +1,32 @@
+#include <algorithm>
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int n;
+vector<pair<int, int>> arr;
+vector<int> L, R;
+
+void input() {
+    cin >> n;
+    arr = vector<pair<int, int>>(n);
+    L = R = vector<int>(n, 0);
+    for (auto &&i : arr)
+        cin >> i.first >> i.second;
+    sort(arr.begin(), arr.end());
+
+    L[0] = arr[0].second, R[n - 1] = arr[n - 1].second;
+    for (int i = 1; i < n; i++) {
+        L[i] = max(L[i - 1], arr[i].second);
+        R[n - 1 - i] = min(R[n - i], arr[n - 1 - i].second);
+    }
+}
+
+int main() {
+    input();
+    int ans = 0;
+    for (int i = 0; i < n; i++)
+        ans += L[i] != R[i];
+    cout << ans;
+    return 0;
+}
