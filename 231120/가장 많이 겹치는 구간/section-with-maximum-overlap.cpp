122b7ed3d5dccd0574c51c752de7b107b1050026
YeoDJ <98148615+YeoDJ@users.noreply.github.com>
2023-11-20 16:20:29 +0900

    Title: 가장 많이 겹치는 구간, Time: 211ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 231120/가장 많이 겹치는 구간/section-with-maximum-overlap.cpp
@@ -0,0 +1,33 @@
+#include <algorithm>
+#include <iostream>
+#include <queue>
+#include <set>
+using namespace std;
+
+int n;
+vector<pair<int, int>> arr;
+set<pair<int, int>> points;
+
+void input() {
+    cin >> n;
+    arr = vector<pair<int, int>>(n);
+    for (auto &&i : arr)
+        cin >> i.first >> i.second;
+
+    // 시작점, 끝점 선언
+    for (auto &&i : arr) {
+        points.insert({i.first, 1});
+        points.insert({i.second, -1});
+    }
+}
+
+int main() {
+    input();
+    int ans = 0, cnt = 0;
+    for (auto &&i : points) {
+        cnt += i.second;
+        ans = max(ans, cnt);
+    }
+    cout << ans;
+    return 0;
+}
