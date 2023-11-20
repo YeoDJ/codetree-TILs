ee265f329230bcad125b1eda1516d3e2202fa5ab
YeoDJ <98148615+YeoDJ@users.noreply.github.com>
2023-11-20 20:41:07 +0900

    Title: 가장 많이 겹치는 구간 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 231120/가장 많이 겹치는 구간 2/section-with-maximum-overlap-2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <set>
+#include <unordered_set>
+using namespace std;
+
+int n;
+vector<pair<int, int>> arr;
+set<tuple<int, int, int>> points;
+
+void input() {
+    cin >> n;
+    arr = vector<pair<int, int>>(n);
+    for (auto &&i : arr)
+        cin >> i.first >> i.second;
+
+    // (시작점, 1, idx), (끝점, -1, idx)
+    for (int i = 0; i < n; i++) {
+        points.insert({arr[i].first, 1, i});
+        points.insert({arr[i].second, -1, i});
+    }
+}
+
+int main() {
+    input();
+    // 시작점을 지나친다면 insert, 끝점을 지나친다면 erase
+    int ans = 0, a, b, idx;
+    unordered_set<int> uset;
+    for (auto &&i : points) {
+        tie(a, b, idx) = i;
+        if (b == 1) uset.insert(idx);
+        else uset.erase(uset.find(idx));
+        ans += uset.empty();
+    }
+    cout << ans;
+    return 0;
+}
