29a1521d8c8c4add8459804763477e623a545f76
YeoDJ <98148615+YeoDJ@users.noreply.github.com>
2023-11-21 14:06:34 +0900

    Title: 최대 구간의 크기, Time: 305ms, Memory: 13MB, Status: Passed - CodeTree

---
+++ 231121/최대 구간의 크기/size-of-largest-interval.cpp
@@ -0,0 +1,43 @@
+#include <algorithm>
+#include <iostream>
+#include <queue>
+#include <set>
+#include <unordered_set>
+using namespace std;
+
+int n;
+vector<pair<int, int>> arr;
+set<tuple<int, int, int>> points;
+
+void input() {
+    cin >> n;
+    arr = vector<pair<int, int>>(n);
+    for (auto &&i : arr)
+        cin >> i.first >> i.second;
+
+    for (int i = 0; i < n; i++) {
+        points.insert({arr[i].first, 1, i});
+        points.insert({arr[i].second, -1, i});
+    }
+}
+
+int main() {
+    input();
+    unordered_set<int> us;
+    int ans = 0, start = 0, a, b, idx;
+    
+    for (auto &&i : points) {
+        tie(a, b, idx) = i;
+        if (b == 1) {
+            us.insert(idx);
+            if (us.size() == 1)
+                start = a;
+        } else {
+            us.erase(idx);
+            if (us.empty())
+                ans = max(ans, a - start);
+        }
+    }
+    cout << ans;
+    return 0;
+}
