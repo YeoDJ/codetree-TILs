3d25b4441eabe900fd05566039449e3c46ad28b1
YeoDJ <98148615+YeoDJ@users.noreply.github.com>
2023-11-22 00:41:03 +0900

    Title: 컴퓨터 이용시간, Time: 203ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 231122/컴퓨터 이용시간/computer-hours.cpp
@@ -0,0 +1,48 @@
+#include <algorithm>
+#include <iostream>
+#include <queue>
+#include <set>
+#include <tuple>
+#define END use_set.end()
+using namespace std;
+
+int n;
+vector<pair<int, int>> arr;
+set<tuple<int, int, int>> points;
+// "대기 중"인 computer 목록
+priority_queue<int, vector<int>, greater<int>> computers;
+
+void input() {
+    cin >> n;
+    arr = vector<pair<int, int>>(n);
+    for (auto &&i : arr)
+        cin >> i.first >> i.second;
+
+    for (int i = 0; i < n; i++) {
+        computers.push(i + 1);
+        points.insert({arr[i].first, 1, i});
+        points.insert({arr[i].second, -1, i});
+    }
+}
+
+vector<int> solution() {
+    vector<int> user_arr(n, 0);
+    int a, b, idx;
+    for (auto &&i : points) {
+        tie(a, b, idx) = i;
+        if (b == 1) {
+            user_arr[idx] = computers.top();
+            computers.pop();
+        } else
+            computers.push(user_arr[idx]);
+    }
+    return user_arr;
+}
+
+int main() {
+    input();
+    vector<int> user_arr = solution();
+    for (auto &&i : user_arr)
+        cout << i << ' ';
+    return 0;
+}
