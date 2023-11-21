c74a15f04f7005054a4804b841b9130901369b3b
YeoDJ <98148615+YeoDJ@users.noreply.github.com>
2023-11-21 20:10:11 +0900

    Title: 컴퓨터 이용시간, Time: 12ms, Memory: 0MB, Status: Runtime Error - CodeTree

--- 231121/컴퓨터 이용시간/computer-hours.cpp
+++ 231121/컴퓨터 이용시간/computer-hours.cpp
@@ -34,7 +34,7 @@ vector<int> solution() {
             auto it2 = --it1;
             auto it3 = END;
             it1++, it3--;
-            com = (use_set.empty() || (it2 == END && *it1 > 1)) ? 1 : (*it1 - *it2 > 1) ? *it2 + 1 : *it3 + 1;
+            com = (use_set.empty() || (it2 == END && it1 != END && *it1 > 1)) ? 1 : (*it1 - *it2 > 1) ? *it2 + 1 : *it3 + 1;
             use_set.insert(com);
             user_arr[idx] = com;
         } else
