d93435704cf8c3e0a28cc7447ce4d11c4ace7c0a
YeoDJ <98148615+YeoDJ@users.noreply.github.com>
2023-12-08 00:03:49 +0900

    Title: 호텔 예약, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 231208/호텔 예약/reserve-hotel.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <queue>
+#include <tuple>
+#include <unordered_set>
+using namespace std;
+
+int n;
+set<tuple<int, int, int>> arr;
+
+void input() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int a, b;
+        cin >> a >> b;
+        arr.insert({a, 1, i});
+        arr.insert({b, -1, i});
+    }
+}
+
+int main() {
+    input();
+    unordered_set<int> us;
+    int a, b, idx, cnt = 0;
+
+    for (auto &&i : arr) {
+        tie(a, b, idx) = i;
+        if (b == 1)
+            us.insert(idx);
+        else
+            us.erase(idx);
+        cnt += us.empty();
+    }
+    cout << cnt;
+    return 0;
+}
