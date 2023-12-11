bc0ae2cafcf11a970761e5250d547222c5a1de58
YeoDJ <98148615+YeoDJ@users.noreply.github.com>
2023-12-11 21:09:12 +0900

    Title: 괄호 쌍 만들어주기, Time: 10ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231211/괄호 쌍 만들어주기/pair-parentheses.cpp
@@ -0,0 +1,32 @@
+#include <algorithm>
+#include <iostream>
+#include <queue>
+#include <string>
+using namespace std;
+
+int n;
+string str;
+vector<int> R;
+
+void input() {
+    cin >> str;
+    n = str.size();
+    R = vector<int>(n, 0);
+
+    int cnt = str[n - 1] == ')';
+    for (int i = n - 2; i >= 0; i--) {
+        cnt = (str[i] == ')') ? cnt + 1 : 0;
+        R[i] = (cnt >= 2) ? R[i + 1] + 1 : R[i + 1];
+    }
+}
+
+int main() {
+    input();
+    int cnt = str[0] == '(', ans = 0;
+    for (int i = 1; i < n; i++) {
+        cnt = (str[i] == '(') ? cnt + 1 : 0;
+        ans += (cnt >= 2) ? R[i] : 0;
+    }
+    cout << ans;
+    return 0;
+}
