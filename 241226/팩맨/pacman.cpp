c1a138af8c5c7e7f6b524be7b4d8a43ab89b478a
YeoDJ <98148615+YeoDJ@users.noreply.github.com>
2024-12-26 23:38:27 +0900

    Title: 팩맨, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

--- 241226/팩맨/pacman.cpp
+++ 241226/팩맨/pacman.cpp
@@ -1,72 +1,65 @@
 #include <iostream>
+#include <map>
 #include <queue>
 #define fastio ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
-#define SZ 4
 
 using namespace std;
 using pii = pair<int, int>;
 
-int m, t;
-int dy[8] = {-1, -1, 0, 1, 1, 1, 0, -1};
-int dx[8] = {0, -1, -1, -1, 0, 1, 1, 1};
+struct monsterInfo {
+    int row, col, dir;
+};
 
-vector<vector<int>> deadBody(4, vector<int>(4, 0));
-pair<int, int> pacman;
-vector<pii> path_tmp;
+int m, t, idx = 0;
+int dy[] = {-1, -1, 0, 1, 1, 1, 0, -1};
+int dx[] = {0, -1, -1, -1, 0, 1, 1, 1};
+
+pii pacman;                   // 팩맨 현재 좌표
+map<int, monsterInfo> ghost;  // idx별 몹 정보
+map<int, int> MAP[4][4];      // 좌표 별 몹 정보(idx, 방향향)
+map<int, int> deadBody[4][4]; // 좌표 별 시체 정보(idx, 시체가 있는 최대 턴)
+vector<pii> path_tmp;         // 백트래킹을 위한 변수
 
 bool inRange(int y, int x) { return 0 <= y && y < 4 && 0 <= x && x < 4; }
 
-class ghostInfo {
-  public:
-    bool isEgg = false;
-    int row, col, dir;
-    int die_cnt = 0; // 시체 소멸 몇번째 턴에 발생하는가(0이면 소멸 대상 아님)
-
-  public:
-    ghostInfo(int r, int c, int d) {
-        this->row = r;
-        this->col = c;
-        this->dir = d;
-    };
-
-    void move() {
-        for (int i = 0; i < 8; i++) {
-            int ny = row + dy[dir];
-            int nx = col + dx[dir];
-            if (inRange(ny, nx) && !deadBody[ny][nx] && make_pair(ny, nx) != pacman) {
-                row = ny, col = nx;
-                break;
-            }
-            dir = (dir == 7) ? 0 : dir + 1;
-        }
-    }
-};
-
-vector<ghostInfo> ghost;
-
 void input() {
     cin >> m >> t;
-    int r, c, d;
-    cin >> r >> c;
-    pacman = {r - 1, c - 1};
+    cin >> pacman.first >> pacman.second;
+    pacman.first--, pacman.second--;
     for (int i = 0; i < m; i++) {
+        int r, c, d;
         cin >> r >> c >> d;
-        ghost.push_back(ghostInfo(r - 1, c - 1, d - 1));
+        monsterInfo tmp = {--r, --c, --d};
+        ghost[idx] = tmp;
+        MAP[r][c][idx++] = d;
     }
 }
 
+// pos번째 몬스터 이동
+void moveMonster(int pos, monsterInfo &g) {
+    for (int i = 0; i < 8; i++) {
+        int ny = g.row + dy[g.dir];
+        int nx = g.col + dx[g.dir];
+        if (inRange(ny, nx) && deadBody[ny][nx].empty() && make_pair(ny, nx) != pacman) {
+            MAP[g.row][g.col].erase(pos);
+            g.row = ny, g.col = nx;
+            MAP[ny][nx][pos] = g.dir;
+            break;
+        }
+        g.dir = (g.dir == 7) ? 0 : g.dir + 1;
+    }
+}
+
+// 팩맨 이동
 void movePacman(pii spos, int lvl, vector<pii> &path, int &cnt) {
     if (lvl == 3) {
-        // 알 상태가 아니며 왔던 좌표에 다시 돌아왔다면 카운트하지 않는다.
-        int tmp = 0;
-        for (int i = 0; i < 3; i++) {
-            if ((i == 1 && path_tmp[i] == pacman) || (i == 2 && path_tmp[i] == path_tmp[0]))
-                continue;
-            for (auto &&j : ghost) {
-                if (j.isEgg)
-                    break;
-                tmp += (path_tmp[i] == make_pair(j.row, j.col));
-            }
+        int tmp = 0, pos = 0;
+        for (auto &&i : path_tmp) {
+            // 왔던 좌표에 다시 돌아왔다면 카운트하지 않는다.
+            if (pos == 2 && i == *path_tmp.begin())
+                break;
+            tmp += MAP[i.first][i.second].size();
+            pos++;
         }
 
         // 가장 많이 먹을 수 있는 경우라면 path 수정
@@ -80,10 +73,9 @@ void movePacman(pii spos, int lvl, vector<pii> &path, int &cnt) {
     for (int i = 0; i < 7; i += 2) {
         int ny = spos.first + dy[i];
         int nx = spos.second + dx[i];
-        pii pos(ny, nx);
         if (inRange(ny, nx)) {
-            path_tmp.push_back(pos);
-            movePacman(pos, lvl + 1, path, cnt);
+            path_tmp.push_back({ny, nx});
+            movePacman({ny, nx}, lvl + 1, path, cnt);
             path_tmp.pop_back();
         }
     }
@@ -95,22 +87,14 @@ int main() {
     input();
 
     for (int i = 0; i < t; i++) {
-        // 몬스터 복제 시도
-        int sz = ghost.size();
-        for (int j = 0; j < sz; j++)
-            if (!ghost[j].die_cnt) {
-                ghostInfo tmp = ghost[j];
-                tmp.isEgg = true;
-                ghost.push_back(tmp);
-            }
+        // 몬스터 복제(알 상태)
+        map<int, monsterInfo> egg;
+        for (auto &&j : ghost)
+            egg[idx++] = j.second;
 
         // 몬스터 이동
-        for (auto &&j : ghost) {
-            if (j.isEgg)
-                break;
-            if (!j.die_cnt)
-                j.move();
-        }
+        for (auto &&j : ghost)
+            moveMonster(j.first, j.second);
 
         // 팩맨 이동
         int cnt = 0;
@@ -119,35 +103,28 @@ int main() {
         movePacman(pacman, 0, path, cnt);
         pacman = path[2];
 
-        // 몬스터 먹어치우기
+        // 몬스터 먹기
         for (auto &&j : path)
-            for (auto &&k : ghost) {
-                if (k.isEgg)
-                    break;
-                if (!k.die_cnt && j == make_pair(k.row, k.col)) {
-                    k.die_cnt = i + 2;
-                    deadBody[k.row][k.col]++;
-                }
+            for (auto &&k : MAP[j.first][j.second]) {
+                deadBody[j.first][j.second][k.first] = i + 2;
+                ghost.erase(k.first);
+                MAP[j.first][j.second].erase(k.first);
             }
 
-        // 시체 소멸
-        for (int j = 0; j < ghost.size(); j++) {
-            if (0 < ghost[j].die_cnt && ghost[j].die_cnt < i) {
-                ghost.erase(ghost.begin() + j);
-                deadBody[ghost[j].row][ghost[j].col]--;
-                j--;
-            }
+        // 시체 치우기
+        for (int y = 0; y < 4; y++)
+            for (int x = 0; x < 4; x++)
+                for (auto &&j : deadBody[y][x])
+                    if (i > j.second)
+                        deadBody[y][x].erase(j.first);
+
+        // 몬스터 복제(알 -> MAP으로 옮기기)
+        for (auto &&j : egg) {
+            ghost[j.first] = j.second;
+            MAP[j.second.row][j.second.col][j.first] = j.second.dir;
         }
-
-        // 몬스터 복제
-        for (auto &&j : ghost)
-            if (j.isEgg)
-                j.isEgg = false;
     }
 
-    int ans = 0;
-    for (auto &&i : ghost)
-        ans += !i.die_cnt;
-    cout << ans;
+    cout << ghost.size();
     return 0;
 }
