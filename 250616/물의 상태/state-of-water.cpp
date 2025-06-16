7e19d39d645548da7835f17804a4dd597fe99e5b
YeoDJ <98148615+YeoDJ@users.noreply.github.com>
2025-06-16 11:07:56 +0900

    Title: 물의 상태, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250616/물의 상태/state-of-water.cpp
+++ 250616/물의 상태/state-of-water.cpp
@@ -4,6 +4,6 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    cout << ((n < 0) ? "ice" : (n >= 100) ? "vapor" : "water")
+    cout << ((n < 0) ? "ice" : (n >= 100) ? "vapor" : "water");
     return 0;
 }
