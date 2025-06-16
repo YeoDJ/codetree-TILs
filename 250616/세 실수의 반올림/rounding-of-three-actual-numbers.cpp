3e0d8834f39057d0baa8e2e4456f4d5b91265c93
YeoDJ <98148615+YeoDJ@users.noreply.github.com>
2025-06-16 10:52:38 +0900

    Title: 세 실수의 반올림, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250616/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float a, b, c;
+    cin >> a >> b >> c;
+    printf("%.3f\n", a);
+    printf("%.3f\n", b);
+    printf("%.3f\n", c);
+    return 0;
+}
