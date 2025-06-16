07d8a02586f6b7299c35dad79f9785ee07488a64
YeoDJ <98148615+YeoDJ@users.noreply.github.com>
2025-06-16 11:16:26 +0900

    Title: 살 수 있는 물건, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250616/살 수 있는 물건/things-able-to-buy.cpp
+++ 250616/살 수 있는 물건/things-able-to-buy.cpp
@@ -4,6 +4,6 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    cout << ((n >= 3000) ? "book" : (n >= 1000) : "mask" : "no");
+    cout << ((n >= 3000) ? "book" : (n >= 1000) ? "mask" : "no");
     return 0;
 }
