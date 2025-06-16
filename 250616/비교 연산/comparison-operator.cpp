272df4d62c3209c69caa31e3efb5ca52c5e86578
YeoDJ <98148615+YeoDJ@users.noreply.github.com>
2025-06-16 11:00:03 +0900

    Title: 비교 연산, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250616/비교 연산/comparison-operator.cpp
+++ 250616/비교 연산/comparison-operator.cpp
@@ -5,11 +5,11 @@ int main() {
     int a, b;
     cin >> a >> b;
     
-    cout << a >= b << endl;
-    cout << a > b << endl;
-    cout << a <= b << endl;
-    cout << a < b << endl;
-    cout << a == b << endl;
-    cout << a != b << endl;
+    cout << (a >= b) << endl;
+    cout << (a > b) << endl;
+    cout << (a <= b) << endl;
+    cout << (a < b) << endl;
+    cout << (a == b) << endl;
+    cout << (a != b) << endl;
     return 0;
 }
