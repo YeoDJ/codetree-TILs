d80920ab540ac4b9174c4fda9e4b46607cfe5422
YeoDJ <98148615+YeoDJ@users.noreply.github.com>
2025-06-16 11:20:00 +0900

    Title: 등급 매기기, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250616/등급 매기기/ranking.cpp
+++ 250616/등급 매기기/ranking.cpp
@@ -4,6 +4,6 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    cout << (char)((n < 60) ? 'F' : 'A' + (9 - (n / 90)));
+    cout << (char)((n < 60) ? 'F' : (n == 100) ? 'A' : 'A' + (9 - (n / 10)));
     return 0;
 }
