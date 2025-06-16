bb00425b5f531ed892edf1e3c162fe1b78f6e116
YeoDJ <98148615+YeoDJ@users.noreply.github.com>
2025-06-16 10:28:04 +0900

    Title: 달에서 무게 구하기, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250616/달에서 무게 구하기/weight-on-the-moon.cpp
+++ 250616/달에서 무게 구하기/weight-on-the-moon.cpp
@@ -4,6 +4,6 @@ using namespace std;
 int main() {
     int weight = 13;
     float gravity_ratio = 0.165;
-    printf("%.6f", weight * gravity_ratio);
+    printf("%d * %.6f = %.6f", weight, gravity_ratio, weight * gravity_ratio);
     return 0;
 }
